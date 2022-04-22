7d706ed4f7323fb2e7d6091788ecc5227065b811
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-04-22 10:50:59 -0400

    feat: implement test for Home page

---
+++ src/tests/pages/Home.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import Home from '../../pages';
+
+describe('Home page tests', () => {
+  it('renders correctly text', () => {
+    const { getByText } = render(<Home />);
+
+    const text = getByText('Front-end template');
+
+    expect(text).toBeInTheDocument();
+  });
+});
